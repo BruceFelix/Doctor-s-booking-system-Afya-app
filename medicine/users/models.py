e527a424b0873f44a002beae36509e3a774c4b46
BruceFelix <brucefelixm@gmail.com>
2022-11-10 15:44:33 +0300

    working on refactoring my models

--- medicine/users/models.py
+++ medicine/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from phone_field import PhoneField
-
 # Create your models here.
 class Person(models.Model):
     GENDER_CHOICES = (
@@ -12,8 +11,7 @@ class Person(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     email = models.EmailField(unique=True)
     # profile_pic = models.ImageField(null=True, blank=True)
-    phone_number = PhoneField(blank=True, help_text='Contact phone number')
-
+    # phone = PhoneField(blank=True, help_text='Contact phone number')
 class Doctor(Person):
     CITY_OPTIONS =(
         ('N', 'Nairobi'),
@@ -22,10 +20,14 @@ class Doctor(Person):
     Speciality = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.firstname +" " + self.lastname
+
+
 class Patient(Person):
     date_of_birth = models.DateField(null=True)
     def __str__(self):
-        return super().__str__
+        return self.firstname +" " + self.lastname
 
 class Appointment(models.Model):
     scheduled_time = models.DateField()
