520f114d62274c5c22469565dbe1949d365469d6
BruceFelix <brucefelixm@gmail.com>
2022-11-14 16:43:14 +0300

    frontend is done working on models

--- afyaapp/urls.py
+++ afyaapp/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from authentication import views as v
 
 urlpatterns = [
-    # path('register/', v.register, name="register"),
-    path('authentication/', include('authentication.urls')),
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def landing(request):
@@ -10,3 +12,13 @@ def doctor(request):
 def patient(request):
     return render(request, 'main/patients.html')
 
+def sign_up(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/landing')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/sign_up.html', {'form':form})
