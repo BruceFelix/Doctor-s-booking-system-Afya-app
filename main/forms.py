b16a84a1cd27af344f0872c28cd8d0a0e500d648
BruceFelix <brucefelixm@gmail.com>
2022-11-22 09:29:17 +0300

    work in progress

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,49 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import *
+
+# class RegistrationForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#     first_name = forms.CharField(required=True)
+#     last_name = forms.CharField(required=True)
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 
-class RegistrationForm(UserCreationForm):
+class PatientForm(UserCreationForm):
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    ]
+
+    username = forms.CharField(
+        max_length=200,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Username'})
+    )
+    password1 = forms.CharField(
+        required = True,
+        widget=forms.PasswordInput(attrs={'class': 'input-field', 'placeholder': 'Password'}),
+        )
+    password2 = forms.CharField(
+        required = True,
+        widget=forms.PasswordInput(attrs={'class': 'input-field', 'placeholder': 'Password Again'}),
+        )
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-
+    phone = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Enter your phone number'}))
+    gender = forms.CharField(label='Choose your Gender', widget=forms.Select(choices=GENDER_CHOICES, attrs={'class': 'input-field'}), required=True)
+    age = forms.CharField(widget=forms.TextInput(attrs={'class': 'input-field', 'id':'birth_date', 'placeholder': 'Enter your age', 'autocomplete': 'off'}), required=True)
+    
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'phone', 'gender', 'age']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'class': 'input-field', 'placeholder': 'Enter Email'})
+        self.fields['first_name'].widget.attrs.update({'class': 'input-field', 'placeholder': 'Enter First Name'})
+        self.fields['last_name'].widget.attrs.update({'class': 'input-field', 'placeholder': 'Enter Last Name'})
--- main/views.py
+++ main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render,redirect
-from . forms import RegistrationForm
+from . forms import PatientForm
 from django.contrib.auth import login, logout, authenticate
-from .models import MyBaseUser, Doctor, Patient
-
+from . models import *
 # Create your views here.
 def landing(request):
     return render(request, 'main/landing.html')
@@ -15,43 +14,69 @@ def patient(request):
 
 def sign_up(request):
     if request.method == "POST":
-        form = RegistrationForm(request.POST)
+        form = PatientForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('/landing')
     else:
-        form = RegistrationForm()
+        form = PatientForm()
     return render(request, 'registration/sign_up.html', {'form':form})
     
 def signup(request):
     return render(request, 'registration/signup.html', )
 
 def patient_signup(request):
-    if request.method == "POST":
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
-        email = request.POST['mail']
-        phonenumber = request.POST['number']
-        age = request.POST['age']
-        gender = request.POST['gender']
-        password = request.POST['password']  
+    # if request.method == "POST":
+    #     firstname = request.POST['firstname']
+    #     lastname = request.POST['lastname']
+    #     email = request.POST['mail']
+    #     phonenumber = request.POST['number']
+    #     age = request.POST['age']
+    #     gender = request.POST['gender']
+    #     password = request.POST['password']  
 
-        newpatient = MyBaseUser(
-            first_name = firstname,
-            last_name = lastname,
-            email = email,
-            gender = gender,
-            mobile_number = phonenumber,
-            is_patient = True, 
-            username= firstname + " " +lastname,
-            password = password,
-            )
-        newpatient.save()
-        patient = Patient(age = age, patientuser = newpatient)
-        patient.save()
-        
-    return render(request, 'registration/patientsignup.html')
+    #     newpatient = MyBaseUser(
+    #         first_name = firstname,
+    #         last_name = lastname,
+    #         email = email,
+    #         gender = gender,
+    #         mobile_number = phonenumber,
+    #         is_patient = True, 
+    #         username= firstname + " " +lastname,
+    #         password = password,
+    #         )
+    #     newpatient.save()
+    #     patient = Patient(age = age, patientuser = newpatient)
+    #     patient.save()
+    if request.method == 'POST':
+        form = PatientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            phone = form.cleaned_data.get('phone')
+            gender = form.cleaned_data.get('gender')
+            age = form.cleaned_data.get('age')
+            first_name = form.cleaned_data.get("first_name")
+            last_name = form.cleaned_data.get("last_name")
+            email = form.cleaned_data.get("email")
+
+            user_patient = User.objects.get(username=username)
+            patient_data = Patient.objects.create(
+                username = first_name,
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                patient_user = user_patient,
+                is_patient = True,
+                mobile_number=phone,
+                gender=gender,
+                age=age)
+            patient_data.save()
+            return render(request, 'registration/login.html')
+    else:
+        form =PatientForm()
+    return render(request, 'registration/patientsignup.html', {'form': form})
 
 def doctor_signup(request):
     if request.method == "POST":
