06e1c1d1e51c51c9ec17e16e761dfc0a8cb8d26b
BruceFelix <brucefelixm@gmail.com>
2022-12-05 13:29:52 +0300

    deploying on railway

--- afyaapp/settings.py
+++ afyaapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c)z-jzsbdtvug&j)ex6^*qjh)x9_88*zso2x4oobh!tja^480_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
